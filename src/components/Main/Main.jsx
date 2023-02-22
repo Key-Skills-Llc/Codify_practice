2b77cf447c7846d99f4512e2195ead376cf1e570
Nechaev <maks25082003@gmail.com>
2023-02-23 00:20:27 +0600

    footer styled

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,67 +1,73 @@
 import React from "react";
-import image from "../../image/avtokursy 1.jpg"
-import metodichka from "../../image/Metodichka.png"
-import Photo from "../../image/photoasd.jpg"
+import image from "../../image/avtokursy 1.jpg";
+import metodichka from "../../image/Metodichka.png";
+import Photo from "../../image/photoasd.jpg";
 
- 
+import { StyledMain } from "./styled";
+
+
+
 const Main = () => {
   return (
     <>
-      <div className="Main-page">
-        <div className="Main-h1">
+      <StyledMain.MainPage>
+        <StyledMain.MainH1>
           <h1>Профессиональный инструктор по вождению</h1>
           <button className="btn-1">Пройти тест онлайн</button>
-        </div>
-        <div>
+        </StyledMain.MainH1>
+        <StyledMain.MainCar>
           <img src={image} alt="car" />
-        </div>
-      </div>
-      <div className="main-biography">
-        <div className="static">
-          <h2>Омурбеков Руслан Жакшылыкович- Инструктор по вождению</h2>
+        </StyledMain.MainCar>
+      </StyledMain.MainPage>
+      <StyledMain.Biography>
+        <StyledMain.Static>
+          <StyledMain.MainH2>
+            Омурбеков Руслан Жакшылыкович- Инструктор по вождению
+          </StyledMain.MainH2>
           <div>
-            <p>
-              <span></span>Водительский стаж <br /> более 25 лет.
-            </p>
-            <p>
-              <span></span>Индивидуально подготовил к практическому <br />{" "}
-              вождению более 1500 студентов
-            </p>
-            <p>
-              <span></span>Опыт преподавания и <br /> инструкторский стаж более
-              12 лет.
-            </p>
-            <p>
-              <span></span>За 6 лет преподавания в автошколе <br /> выпустил
-              более 1600 студентов
-            </p>
+            <StyledMain.MainItemCommet>
+              <StyledMain.Matches></StyledMain.Matches>Водительский стаж <br />{" "}
+              более 25 лет.
+            </StyledMain.MainItemCommet>
+            <StyledMain.MainItemCommet>
+              <StyledMain.Matches></StyledMain.Matches>Индивидуально подготовил
+              к практическому <br /> вождению более 1500 студентов
+            </StyledMain.MainItemCommet>
+            <StyledMain.MainItemCommet>
+              <StyledMain.Matches></StyledMain.Matches>Опыт преподавания и{" "}
+              <br /> инструкторский стаж более 12 лет.
+            </StyledMain.MainItemCommet>
+            <StyledMain.MainItemCommet>
+              <StyledMain.Matches></StyledMain.Matches>За 6 лет преподавания в
+              автошколе <br /> выпустил более 1600 студентов
+            </StyledMain.MainItemCommet>
 
             <button className="btn-2">Записаться на вождение</button>
           </div>
-        </div>
-        <div >
+        </StyledMain.Static>
+        <div>
           <img src={Photo} alt="fgjsfjsgfj" />
         </div>
-      </div>
+      </StyledMain.Biography>
       <div>
-        <hr />
+        <StyledMain.Line />
       </div>
-      <div className="main-manual">
+      <StyledMain.MainManual>
         <div>
           <img src={metodichka} alt="manual" />
         </div>
-        <div className="main-manual-posob">
+        <StyledMain.MainManualPosob>
           <h4>Методическое пособие</h4>
-          <p>
+          <StyledMain.MainItemCommet>
             <span></span>Во время преподавания ПДД в автошколе, обнаружил ошибки
             и не полную информацию в ПДД Кыргызстана от 5.02.2020 года.
             Отталкиваясь от этих ошибок, составил свою методичку по ПДД
-          </p>
-          <h6>Стоимость методички</h6>
+          </StyledMain.MainItemCommet>
+          <StyledMain.MainH6>Стоимость методички</StyledMain.MainH6>
           <b>250 сом</b>
           <button className="btn-2">Купить методичку</button>
-        </div>
-      </div>
+        </StyledMain.MainManualPosob>
+      </StyledMain.MainManual>
     </>
   );
 };
--- src/components/Main/styled.js
+++ src/components/Main/styled.js
@@ -1,2 +1,148 @@
 import styled from "styled-components";
 
+export const StyledMain = {
+  MainPage: styled.div`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin-top: 93px;
+    margin-left: 155px;
+
+    font-style: normal;
+    font-weight: 600;
+    font-size: 25px;
+    line-height: 61px;
+    color: #2e3538;
+  `,
+  MainCar: styled.div`
+    left: 500px;
+  `,
+  MainH1: styled.div`
+    width: 576px;
+    margin-bottom: 109px;
+  `,
+  Biography: styled.div`
+    display: flex;
+    margin-left: 150px;
+    margin-top: 78px;
+    margin-bottom: 79px;
+
+    img {
+      width: 367px;
+      height: 481px;
+      margin-left: 129px;
+    }
+  `,
+  Static: styled.div``,
+  MainH2: styled.h2`
+    width: 560px;
+    font-weight: 700;
+    font-size: 25px;
+    line-height: 45px;
+    letter-spacing: 0.03em;
+    color: #2e3538;
+    margin-bottom: 51px;
+  `,
+  MainItemCommet: styled.p`
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    padding: 0;
+    margin-bottom: 20px;
+    font-weight: 500;
+    font-size: 20px;
+
+    width: 560px;
+    height: 70px;
+
+    background: linear-gradient(
+      90deg,
+      rgba(148, 197, 45, 0.2) 0%,
+      rgba(42, 145, 52, 0) 100%
+    );
+    border-radius: 5px;
+  `,
+  Matches: styled.span`
+    width: 5px;
+    height: 70px;
+    margin-right: 10px;
+
+    background: #94c52d;
+    box-shadow: 0px 0px 10px rgba(148, 197, 45, 0.5);
+    border-radius: 100px;
+
+    flex: none;
+    order: 0;
+    flex-grow: 0;
+  `,
+  Line: styled.hr`
+    position: absolute;
+    width: 100%;
+    height: 0px;
+    margin: 0;
+    border: 1px solid #454f54;
+    transform: rotate(180deg);
+  `,
+  MainManual: styled.div`
+    display: flex;
+    margin-left: 150px;
+    padding-top: 45px;
+
+    img {
+      width: 452px;
+      height: 524px;
+      margin-top: 3px;
+    }
+  `,
+  MainManualPosob: styled.div`
+    margin-left: 126px;
+
+    h4 {
+      font-weight: 800;
+      font-size: 25px;
+      line-height: 45px;
+      letter-spacing: 0.03em;
+      margin-bottom: 19px;
+      color: #2e3538;
+    }
+
+    p {
+      width: 439px;
+      height: 190px;
+      margin: 0;
+      font-weight: 500;
+      font-size: 22px;
+      line-height: 30px;
+      color: #2e3538;
+
+      background: linear-gradient(
+        90deg,
+        rgba(148, 197, 45, 0.2) 0%,
+        rgba(42, 145, 52, 0) 100%
+      );
+      border-radius: 5px;
+
+      span {
+        width: 5px;
+        height: 190px;
+        margin-right: 10px;
+
+        background: #94c52d;
+        box-shadow: 0px 0px 10px rgba(148, 197, 45, 0.5);
+        border-radius: 100px;
+
+        flex: none;
+        order: 0;
+        flex-grow: 0;
+      }
+    }
+  `,
+  MainH6: styled.h6`
+    font-weight: 400;
+    font-size: 22px;
+    line-height: 27px;
+    color: #2e3538;
+
+    margin: 35px 0px 17px 11px;
+  `,
+};
